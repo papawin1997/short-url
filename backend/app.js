b7dd82198a985bfd526592eef6964da4c155b87d
papawin1997 <papawin1st@gmail.com>
2023-01-06 15:09:01 +0700

    add features

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const crypto = require('crypto');
+
 
 var con = require('./connect')
 var logi = require('./businessLogi')
-
 
 // For parsing application/json
 app.use(express.json());
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.post('/shorturl/create', (req, res) => {
     if(!req.body?.url) return res.send('url is not empty');
     const url = req.body.url;
     const shotURL = logi.generateShortUrl(url);
-    return res.json(shotURL);
+    con.then((db)=>{
+      db.query("SELECT * FROM urls WHERE LongURL = ? " , [url] , (err,result) =>{
+        if (err) {
+          console.log(err)
+          return res.status(400).send(err);
+        }
+        if(result.length > 0) {return res.status(200).json( { message : "The URL already exists." })}
+        else{
+          db.query("INSERT INTO urls(LongURL, ShortURL) VALUES (?,?)",[url,shotURL],(err) => {
+            if (err) {
+              console.log(err)
+              return res.status(400).send();
+            }
+            return res.status(200).json( { message : "successfully" })
+          });
+        }
+      })
+    }).catch((err)=>{return res.send(err)})
 })
 
-app.get('/shortcut/read', (req, res) => {
+app.get('/shorturl/read', (req, res) => {
     if(!req.body?.url) return res.send('url is not empty');
     const url =  new String(req.body?.url);
     con.then((db)=>{
@@ -33,7 +45,6 @@ app.get('/shortcut/read', (req, res) => {
           });
     }).catch((err)=>{return res.send(err)})
 }) 
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- backend/businessLogi.js
+++ backend/businessLogi.js
@@ -1,10 +1,11 @@
+const crypto = require('crypto');
 module.exports = {
     generateShortUrl : (longUrl) => {
         // Hash the long URL to create a unique short URL
         const hash = crypto.createHash('sha256');
         hash.update(longUrl);
-        const shortUrl = hash.digest('hex').substring(0, 6);
-    
+        const startURL = "www.short-url.com/"
+        const shortUrl = startURL + hash.digest('hex').substring(0, 6);
         // Return the short URL
         return shortUrl;
     }
